062843c05a02074668575072fb7c405512d81f57
Siddharth Karanchery <webdev.siddharth@gmail.com>
2024-01-26 12:58:00 +0530

    made HeroBanner responsive + misc code cleanup

--- src/modules/HeroBanner/HeroBanner.jsx
+++ src/modules/HeroBanner/HeroBanner.jsx
@@ -1,4 +1,5 @@
 import { Box, Grid, Typography } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import * as React from "react";
 import "./HeroBanner.css";
 import axios from "axios";
@@ -7,6 +8,9 @@ import moment from "moment";
 export const HeroBanner = () => {
   const [mockApiData, setMockApiData] = React.useState([]);
   const [nextDrawDate, setNextDrawDate] = React.useState();
+
+  const isMobile = useMediaQuery("(max-width:450px)");
+  const isTablet = useMediaQuery("(max-width:850px)");
 
   React.useEffect(() => {
     axios
@@ -27,52 +31,45 @@ export const HeroBanner = () => {
 
   return (
     mockApiData.length !== 0 && (
-      <Grid
-        container
-        justifyContent="center"
-        direction="row"
-        sx={{ margin: "1rem 0" }}
-      >
-        <Grid item className="root" lg={8}>
-          <Grid item className="HeroBanner" lg={6}>
-            <img
-              src={
-                "https://media.giphy.com/media/MONvE9pT7jsEE/giphy-downsized-large.gif"
-              }
-              alt="logo"
-              className="HeroBanner__flag"
-            />
-          </Grid>
-          <Grid item className="HeroBanner" lg={6}>
-            <Box className="HeroBanner__Content">
-              <Typography sx={{ fontWeight: "bold" }}>
-                Draw No:
-                {`${mockApiData[mockApiData.length - 1].drawNum} (${moment(
-                  mockApiData[mockApiData.length - 1].date
-                ).format("DD MMM YYYY")})`}
-              </Typography>
-              <Box className="HeroBanner__Content__data" flexGrow={1}>
-                <Box className="HeroBanner__Content__data__item">
-                  <Typography variant="h6">Cutoff: </Typography>
-                  <Typography variant="h1">
-                    {mockApiData[mockApiData.length - 1].cutoff}{" "}
-                  </Typography>
-                </Box>
-                <Box className="HeroBanner__Content__data__item">
-                  <Typography variant="h6">ITA's: </Typography>
-                  <Typography variant="h1">
-                    {mockApiData[mockApiData.length - 1].ITA}{" "}
-                  </Typography>
-                </Box>
+      <Box className="HeroBanner">
+        <Box item className="HeroBanner__ele">
+          <img
+            src={
+              "https://media.giphy.com/media/MONvE9pT7jsEE/giphy-downsized-large.gif"
+            }
+            alt="logo"
+            className="HeroBanner__flag"
+          />
+        </Box>
+        <Box item className="HeroBanner__ele">
+          <Box className="HeroBanner__Content">
+            <Typography sx={{ fontWeight: "bold" }}>
+              Draw No:
+              {`${mockApiData[mockApiData.length - 1].drawNum} (${moment(
+                mockApiData[mockApiData.length - 1].date
+              ).format("DD MMM YYYY")})`}
+            </Typography>
+            <Box className="HeroBanner__Content__data" flexGrow={1}>
+              <Box className="HeroBanner__Content__data__item">
+                <Typography variant="h6">Cutoff: </Typography>
+                <Typography variant={isMobile || isTablet ? "h2" : "h1"}>
+                  {mockApiData[mockApiData.length - 1].cutoff}
+                </Typography>
               </Box>
-              <Typography className="HeroBanner__next" variant="caption">
-                Next draw on:
-                {` ${moment(nextDrawDate, "YYYY-MM-DD").format("DD MMM YYYY")}`}
-              </Typography>
+              <Box className="HeroBanner__Content__data__item">
+                <Typography variant="h6">ITA's: </Typography>
+                <Typography variant={isMobile || isTablet ? "h2" : "h1"}>
+                  {mockApiData[mockApiData.length - 1].ITA}
+                </Typography>
+              </Box>
             </Box>
-          </Grid>
-        </Grid>
-      </Grid>
+            <Typography className="HeroBanner__next" variant="caption">
+              Next draw on:
+              {` ${moment(nextDrawDate, "YYYY-MM-DD").format("DD MMM YYYY")}`}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
     )
   );
 };
